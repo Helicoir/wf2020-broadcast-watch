05050a9f92042c79f5ca5cebb2d4f1126d77a70e
Helicoir <shogo,kobayashi@wasedasai.net>
2020-10-03 01:01:04 +0900

    claspによる軽めのバックエンド構築

---
+++ clasp/index.ts
@@ -0,0 +1,18 @@
+const getData = function(sheetName: string) {
+  const sheet = SpreadsheetApp.getActive().getSheetByName(sheetName)
+  if (!sheet) {
+    return null
+  }
+  const rows = sheet.getDataRange().getValues()
+  return rows.map(row => {
+    return {
+      name: row[1],
+      id: row[0],
+      day: row[2]
+    }
+  }).slice(1)
+}
+
+export const doGet = function() {
+  return ContentService.createTextOutput(JSON.stringify(getData('data'), null, 2)).setMimeType(ContentService.MimeType.JSON)
+}
