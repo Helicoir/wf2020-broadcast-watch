bed9b679ce0bfabca3c839013b33fe2454009f64
Helicoir <shogo,kobayashi@wasedasai.net>
2020-10-03 14:17:11 +0900

    next.configの追加

---
+++ next.config.js
@@ -0,0 +1,7 @@
+module.exports = {
+  env: {
+    YOUTUBE_API_CLI_ID: '821547660001-v317lk79m9hqcucs36ifv0tbfm29nmc8.apps.googleusercontent.com',
+    YOUTUBE_API_CLI_SECRET: 'ylwhvKMveXuyYa_xh128IPAU',
+    YOUTUBE_API_API_KEY: 'AIzaSyAsIU7Nw4DJ_TonDKqFfIBCAVjowsZtFos'
+  }
+}
