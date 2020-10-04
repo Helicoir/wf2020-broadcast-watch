eb97c7afde3a78b8c2b2be9eba4306d5101d1853
Helicoir <shogo,kobayashi@wasedasai.net>
2020-10-05 02:29:42 +0900

    動作確認

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,9 +3,13 @@ import styled from 'styled-components'
 import Layout from '../components/Layout'
 import { Video } from '../interfaces'
 import { getVideoListBySheet } from '../utils/youtube'
+const tw = require('tailwind.macro')
 
 const Text = styled.p`
   color: red;
+`
+const VideoContainer = styled.div`
+  ${tw`flex`};
 `
 
 const IndexPage: React.FC = (props) => {
@@ -19,12 +23,12 @@ const IndexPage: React.FC = (props) => {
   return (
     <Layout title="Home | Next.js + TypeScript Example">
       {data && data.map(datum => (
-        <div key={datum.meta.id} className='flex'>
+        <VideoContainer key={datum.meta.id}>
           <Text>name: {datum.meta.title}</Text>
           <p>配信タイトル: {datum.video.title}</p>
-          <img src={datum.video.thumbnails.default.url} alt="サムネイル"/>
+          <img src={datum.video.thumbnails.default.url} alt="サムネイル" />
           <span>{datum.video.liveBroadcastContent === 'live' ? '配信中' : '終了 or 予約'}</span>
-        </div>
+        </VideoContainer>
       ))}
     </Layout>
   )
