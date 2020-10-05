d36da9250b3456f4107ae028645d621d36325d92
Helicoir <shogo,kobayashi@wasedasai.net>
2020-10-06 02:37:48 +0900

    調整

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,14 +5,20 @@ import '../assets/css/reset.css'
 import styled from 'styled-components'
 const tw = require('tailwind.macro')
 
+const WrapComponent = styled.main`
+  ${tw`pt-8`}
+  background: #ECF1F9;
+  font-family: 'Avenir Next', 'Helvetica', 'Hiragino Kaku Gothic ProN', 'Hiragino Sans';
+`
+
 const Title = styled.h1`
-  ${tw`text-6xl font-bold  text-center text-gray-900 p-12`}
+  ${tw`text-6xl font-bold  text-center text-gray-900 pb-8`}
 `
 const Description = styled.p`
   ${tw`text-gray-600 text-center`}
 `
 const Hr = styled.div`
-  ${tw`m-16`}
+  ${tw`m-12`}
 `
 
 const App = ({
@@ -20,13 +26,13 @@ const App = ({
   pageProps
 }: AppProps) => {
   return (
-    <>
+    <WrapComponent>
       <Hr />
       <Title>Broadcast Watch</Title>
       <Description>早稲田祭2020 配信状態監視画面</Description>
       <Hr />
       <Component {...pageProps} />
-    </>
+    </WrapComponent>
   )
 }
 
